553ad2ad6c9b2e89e98fa5ac686987c6adfc2044
khushali452 <khushalivasani.ict19@gmail.com>
2021-07-16 15:54:22 +0530

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
+import SideBar from './SideBar';
 import './App.css';
+import SidebarOption from './SidebarOption';
 
 function App() {
   return (
     <div className="App">
       <h1>hey</h1>
+
+      {/*sidebar */}
+      <SideBar/>
+      {/* feed */}
+      {/* widgets */}
     </div>
   );
 }
