7472d55f43c8cba7b6ba4595a98fd4b1dc9d8185
khushali452 <khushalivasani.ict19@gmail.com>
2021-07-25 23:27:41 +0530

    updated

--- src/Post.js
+++ src/Post.js
@@ -1,6 +1,11 @@
 import { Avatar } from '@material-ui/core';
 import React from 'react';
 import "./Post.css";
+import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import PublishIcon from "@material-ui/icons/Publish";
 
 function Post({
     displayName,
@@ -16,6 +21,21 @@ function Post({
                 <Avatar src="https://www.mockofun.com/wp-content/uploads/2019/12/circle-profile-pic.jpg"/>
             </div>
             <div className="post__body">
+                <div className="post__header">
+                    <div className="post__headerText">
+                        <h3>Khushali Vasani</h3><span>< VerifiedUserIcon className="post__badge" /></span>
+                    </div>
+                    <div className="post__headerDescription">
+                        <p>Hey there!!</p>
+                    </div>
+                    <img src="http://www.reactiongifs.com/r/cheering_minions.gif"></img>
+                <div className="post__footer">
+                <ChatBubbleOutlineIcon fontSize="small" />
+            <RepeatIcon fontSize="small" />
+            <FavoriteBorderIcon fontSize="small" />
+            <PublishIcon fontSize="small" />
+                </div>
+                </div>
                 
             </div>
         </div>
